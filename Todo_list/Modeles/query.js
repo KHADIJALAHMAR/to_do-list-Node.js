eb2d9f411fa9f3eb80350ef235f7712a4d5e3f69
khadija <khadijalahmar8@gmail.com>
2022-01-03 23:46:34 +0100

    create callback function

--- Todo_list/Modeles/query.js
+++ Todo_list/Modeles/query.js
@@ -1,2 +1,20 @@
 const Connection = require("../Database/connexion");
 let database = new Connection();
+
+let getProjects = (callback) => {
+    
+    database.connection.query('SELECT * FROM projects', function(err, res) {
+      if(err) throw err;
+      let projects = [];
+      res.forEach(row => {
+          projects.push( {
+              id_project: row.id_project,
+              title: row.name_project,
+              description: row.descreption
+          }); 
+      });
+      callback(projects);
+      console.log(projects)
+  });
+  
+}

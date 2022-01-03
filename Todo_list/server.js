ee792c3eb83903c64051c9bc3eebc0c676baf0cd
khadija <khadijalahmar8@gmail.com>
2022-01-04 00:08:35 +0100

    readFilsync in render routing page

--- Todo_list/server.js
+++ Todo_list/server.js
@@ -6,37 +6,50 @@ const getProjects =require('../Models/query');
 const pt = require('path');
 
 const server =http.createServer((req, res) =>{
-    console.log(req.url , req.method) ;
-
+    // console.log(req.url , req.method) ;
     // set header content  type
     res.setHeader('Content-Type' , 'text/html');
 
-    let path ='./views/';
+    let path ='/';
     switch (req.url) {
         case '/':
-            path += 'index.html';   
-            console.log('index.html');     
+            path += 'index';   
+            getProjects((projects) => {
+                res.writeHead(200 , {'Content-Type' : 'text/html'});
+                let ejsFile = fs.readFileSync(pt.join(pt.dirname(__dirname), 'views', `${path}.ejs`) , 'utf-8');
+                let ejsContent = ejs.render(ejsFile, {projects: projects})
+                res.end(ejsContent);
+            });
             break;
+            // case '/deleteProject':
+            //     path += 'index';
+            //     deleteProject();
+            //     break;
             case '/about':
-                path += 'about.html';   
-                 console.log('about.html');  
-
+                path  += 'about.ejs';
+                res.statusCode = 200;
                 break;
-    
-    }
-    // send an html file
-fs.readFile(path , (err, data)=>{
-    if(err){
-        console.log(err);
-        res.end();
-    }else{
-        res.write(data);
-        res.end();
-    }
-} 
-)
+            case '/contact':
+                path += 'contact.ejs';
+                res.statusCode = 200;
+                break;
+            // Redirect page
+            case '/about-us':
+                res.statusCode = 301;
+                res.setHeader('Location', '/about');
+                res.end();
+                break;
+            default:
+                path = '404.ejs';
+                res.statusCode = 404;
+                break;
+        }
+        
+    });
 
-});
+
+
+
 server.listen(3000, 'localhost' ,()=>{
     console.log('hiya hadik ');
 });
